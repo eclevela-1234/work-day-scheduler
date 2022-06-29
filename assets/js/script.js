8dff1b5f40dd3e07ff1d9e1520af6a5beeb61f46
Eliot <eliothead@live.com>
2022-06-29 15:35:57 -0400

    textarea started

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 console.log("LET'S ROLL!");
 
+// get time and display in header
 var today = moment().format("dddd, MMMM Do YYYY");
 $("#currentDay").text(today);
 
@@ -7,49 +8,50 @@ $("#currentDay").text(today);
 //     localStorage.setItem("tasks", JSON.stringify(tasks));
 //   };
 
+
 var auditTime = function () {
-  // get date from task element
+
+  // get time text from hour classes
   var times = $(".hour");
   var timeNow = moment().format("ha");
-
+// loop through hour classes and format corresponding description classes
   for (i = 0; i < times.length; i++) {
+    // isolate .hour elements
     var calEl = times[i];
-    console.log("+++++");
     var calendarTime = calEl.innerText;
-    //remove color class
-    // var nextDiv = calEl.closest(".row").find(".description")
-    var timeNowMoment = moment().format("dddd, MMMM Do YYYY ha");
+    // concatenate time with date to create a valid moment input
     var momentString = today.concat(" ", calendarTime)
+    //parse momentString as moment object
     var calMoment = moment(momentString, "dddd, MMMM Do YYYY ha");
-    console.log(timeNowMoment);
-    console.log(momentString);
+    // remove tense classes and compare calendar
     $(calEl).next().removeClass("past present future");
+
     if (timeNow === calendarTime) {
         $(calEl).next().addClass("present");
-    } else if (calMoment.isBefore()){
+    } else if (calMoment.isBefore()) {
         $(calEl).next().addClass("past");
     } else {
         $(calEl).next().addClass("future");
     };
-
-
   }
-  //.find("span").text().trim();
+
 };
+
+$(".time-block").on("click", ".description", function() {
+     var text = $(this)
+ .text()
+ .trim();
+    
+    if (!(this).innerText) {
+        var textInput = $("<textarea>").val(text);
+        $(this).append(textInput);
+      
+        // auto focus new element
+        textInput.trigger("focus");
+};
+
+
+
+});
+
 auditTime();
-
-
-//   // convert to moment object at 5:00pm
-//   var time = moment(date, "L").set("hour", 17);
-
-//   console.log(time);
-
-//   // remove any old classes from element
-//   $(taskEl).removeClass("list-group-item-warning list-group-item-danger");
-
-//   // apply new class if task is near/over due date
-//   if (moment().isAfter(time)) {
-//     $(taskEl).addClass("list-group-item-danger");
-//   } else if (Math.abs(moment().diff(time, "days")) <= 2) {
-//     $(taskEl).addClass("list-group-item-warning");
-//   }
